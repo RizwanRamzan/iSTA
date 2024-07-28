632214cf14bfd121a7b357d98ede91f0e4eee054
ammarjavaid <ammarjavaid014@gmail.com>
2024-07-28 17:09:00 +0500

    changes

--- src/Component/Layout/TopBar.jsx
+++ src/Component/Layout/TopBar.jsx
@@ -1,49 +1,122 @@
-import React, { useEffect, useState } from 'react'
-import { Logo, SearchIcon } from '../../assets'
-import { Input } from 'antd'
-import '../../GeneralStyle/index.scss'
-import './TopBar.scss'
-import { useNavigate } from 'react-router-dom'
-import Link from 'antd/es/typography/Link'
+import React, { useEffect, useState } from "react";
+import { Logo, SearchIcon } from "../../assets";
+import { Drawer, Input, message } from "antd";
+import "../../GeneralStyle/index.scss";
+import "./TopBar.scss";
+import { useNavigate } from "react-router-dom";
+import Link from "antd/es/typography/Link";
+import { useMediaQuery } from "react-responsive";
+import { MenuFoldOutlined } from "@ant-design/icons";
 
 const TopBar = ({ button, activeClass, tabsColor }) => {
+  const [activeTabs, setActiveTabs] = useState("");
+  const [open, setOpen] = useState(false);
 
-  const [activeTabs, setActiveTabs] = useState("")
-
-
-  const navigate = useNavigate()
-  const windowLocation = window.location.pathname
+  const navigate = useNavigate();
+  const windowLocation = window.location.pathname;
 
   const tabs = (tab, path) => {
-    setActiveTabs(tab)
-    navigate(path)
+    setActiveTabs(tab);
+    navigate(path);
+  };
 
-  }
+  const showDrawer = () => {
+    setOpen(true);
+  };
+
+  const onClose = () => {
+    setOpen(false);
+  };
+
+  // Use Media Query
+  const mobileResponsive = useMediaQuery({
+    query: "(max-width: 800px)",
+  });
 
   return (
-    <div className='headerMain'>
-      <div className='header'>
-        <div className='left-side'>
+    <div className="headerMain">
+      <div className="header">
+        <div className="left-side">
           <img src={Logo} />
-          <Input className='ant-input-affix-wrapper' placeholder='Search iSTA...' suffix={<img src={SearchIcon} />} />
+          {mobileResponsive ? null : (
+            <Input
+              className="ant-input-affix-wrapper"
+              placeholder="Search iSTA..."
+              suffix={<img src={SearchIcon} />}
+            />
+          )}
         </div>
-        <div className='right-side'>
-          <div className='tabs'>
-            <p className={windowLocation.includes("home") && activeClass} onClick={() => tabs("home", "/home")}>
-              Home
-            </p>
-            <p className={windowLocation.includes("about") && activeClass} onClick={() => tabs("about", "/about")}>
-              About
-            </p>
-            <p className={windowLocation.includes("contact") && activeClass} onClick={() => tabs("contact", "/contact")}>
-              Contact
-            </p>
-          </div>
-            <button onClick={()=>navigate("/joinig")} className={button == "Join" && "loginButton"}>{button}</button>
-        </div>
+        {mobileResponsive ? (
+          <>
+            <div className="bars">
+              <MenuFoldOutlined
+                onClick={() => showDrawer()}
+                style={{ fontSize: "30px" }}
+              />
+            </div>
+          </>
+        ) : (
+          <>
+            <div className="right-side">
+              <div className="tabs">
+                <p
+                  className={windowLocation.includes("home") && activeClass}
+                  onClick={() => tabs("home", "/home")}
+                >
+                  Home
+                </p>
+                <p
+                  className={windowLocation.includes("about") && activeClass}
+                  onClick={() => tabs("about", "/about")}
+                >
+                  About
+                </p>
+                <p
+                  className={windowLocation.includes("contact") && activeClass}
+                  onClick={() => tabs("contact", "/contact")}
+                >
+                  Contact
+                </p>
+              </div>
+              <button
+                onClick={() => navigate("/joinig")}
+                className={button == "Join" && "loginButton"}
+              >
+                {button}
+              </button>
+            </div>
+          </>
+        )}
       </div>
+      <Drawer
+        width={300}
+        title="Menu"
+        placement="right"
+        onClose={onClose}
+        open={open}
+      >
+        <div className="tabs">
+          <Input
+            className="ant-input-affix-wrapper"
+            placeholder="Search iSTA..."
+            suffix={<img src={SearchIcon} />}
+          />
+          <a href="/home" target="_blank">
+            <p style={{ color: "#3B7DED" }}>Home</p>
+          </a>
+          <a href="/about" target="_blank">
+            <p style={{ color: "#2BA24C" }}>About</p>
+          </a>
+          <a href="/contact" style={{ color: "#E43E2B" }}>
+            Contact
+          </a>
+        </div>
+        <button onClick={() => navigate("/joinig")} className="loginButton">
+          Join
+        </button>
+      </Drawer>
     </div>
-  )
-}
+  );
+};
 
-export default TopBar
+export default TopBar;
--- src/Pages/Contact/index.jsx
+++ src/Pages/Contact/index.jsx
@@ -1,95 +1,149 @@
-import React from 'react'
-import TopBar from '../../Component/Layout/TopBar'
-import './contact.scss'
-import { Button, Card, Col, Form, Input, Row } from 'antd'
-import { Gmail, whatsapp } from '../../assets'
-import Footer from '../../Component/Layout/Footer'
+import React from "react";
+import TopBar from "../../Component/Layout/TopBar";
+import "./contact.scss";
+import { Button, Card, Col, Form, Input, Row } from "antd";
+import { Gmail, whatsapp } from "../../assets";
+import Footer from "../../Component/Layout/Footer";
+import { useMediaQuery } from "react-responsive";
 
 const Contact = () => {
+  const { TextArea } = Input;
 
-    const { TextArea } = Input;
+  const mobileResponsive = useMediaQuery({
+    query: "(max-width: 768px)",
+  });
 
-    return (
-        <div className='contact-main'>
-            <TopBar button="Join" activeClass="activehome" />
-            <div className='contact'>
-                <p>Contact</p>
-            </div>
-            <div className='contact-content'>
-                <div className='contact-text-box'>
-                    <Row gutter={40}>
-                        <Col span={24}>
-                            <p>Connecting with Us is Just a Click Away!</p>
-                        </Col>
-                        <Col span={12}>
-                            <Card className='contact-boxs'>
-                                <img src={whatsapp} />
-                                <p>+92 300 110 4782</p>
-                            </Card>
-                        </Col>
-                        <Col span={12}>
-                            <Card className='contact-boxs'>
-                                <img src={Gmail} />
-                                <p>ista478822@gmail.coms</p>
-                            </Card>
-                        </Col>
-                    </Row>
-                </div>
-            </div>
-            <div className='contact-form'>
-                <div className='contact-form-box'>
-                    <Form layout='vertical' style={{ width: "100%" }}>
-                        <Row gutter={40}>
-                            <Col span={24}><p>Get in Touch With Us</p></Col>
-                            <Col span={12}>
-                                <Form.Item name='name' label="Full Name" rules={[{ required: true, message: "please enter you full name" }]}>
-                                    <Input placeholder='Enter Fisrt Name' className='ant-input-affix-wrapper' />
-                                </Form.Item>
-                            </Col>
-                            <Col span={12}>
-                                <Form.Item name='cnic' label="CNIC (Optional)" rules={[{ required: false, message: "please enter you CNIC" }]}>
-                                    <Input placeholder='Enter CNIC' className='ant-input-affix-wrapper' />
-                                </Form.Item>
-                            </Col>
-                            <Col span={12}>
-                                <Form.Item name='email' label="Email" rules={[{ required: true, message: "please enter you valid email" }]}>
-                                    <Input placeholder='Enter email' className='ant-input-affix-wrapper' />
-                                </Form.Item>
-                            </Col>
-                            <Col span={12}>
-                                <Form.Item name='phoneNumber' label="Phone Number" rules={[{ required: true, message: "please enter you phone number" }]}>
-                                    <Input type='number' placeholder='Enter phone number' className='ant-input-affix-wrapper' />
-                                </Form.Item>
-                            </Col>
-                            <Col span={24} className='textarea'>
-                                <Form.Item name='description' label="Description" rules={[{ required: true, message: "please enter you description" }]}>
-                                    <TextArea rows={4} placeholder='Enter description' className='ant-input-affix-wrapper' />
-                                </Form.Item>
-                            </Col>
-                        </Row>
-                        <div span={24} className='form-button'>
-                            <button>Send message</button>
-                        </div>
-                    </Form>
-                </div>
-            </div>
-
-
-            {/* Meet Our iSTA Team */}
-
-            <div className='meet'>
-                <div className='meet-team' >
-                    <div className='meet-box' >
-                        <p className='meet-our'>Frequently Asked Questions</p>
-                        <p className='meet-explor' >Explore</p>
-                    </div>
-                </div>
-            </div>
-
-            <Footer />
-
+  return (
+    <div className="contact-main">
+      <TopBar button="Join" activeClass="activehome" />
+      <div className="contact">
+        <p>Contact</p>
+      </div>
+      <div className="contact-content">
+        <div className="contact-text-box">
+          <Row gutter={40}>
+            <Col span={24}>
+              <p>Connecting with Us is Just a Click Away!</p>
+            </Col>
+            <Col span={mobileResponsive ? 24 : 12}>
+              <Card className="contact-boxs">
+                <img src={whatsapp} />
+                <p>+92 300 110 4782</p>
+              </Card>
+            </Col>
+            <Col span={mobileResponsive ? 24 : 12} className="mail">
+              <Card className="contact-boxs">
+                <img src={Gmail} />
+                <p>ista478822@gmail.coms</p>
+              </Card>
+            </Col>
+          </Row>
         </div>
-    )
-}
+      </div>
+      <div className="contact-form">
+        <div className="contact-form-box">
+          <Form layout="vertical" style={{ width: "100%" }}>
+            <Row gutter={40}>
+              <Col span={24}>
+                <p>Get in Touch With Us</p>
+              </Col>
+              <Col span={mobileResponsive ? 24 : 12}>
+                <Form.Item
+                  name="name"
+                  label="Full Name"
+                  rules={[
+                    { required: true, message: "please enter you full name" },
+                  ]}
+                >
+                  <Input
+                    placeholder="Enter Fisrt Name"
+                    className="ant-input-affix-wrapper"
+                  />
+                </Form.Item>
+              </Col>
+              <Col span={mobileResponsive ? 24 : 12}>
+                <Form.Item
+                  name="cnic"
+                  label="CNIC (Optional)"
+                  rules={[
+                    { required: false, message: "please enter you CNIC" },
+                  ]}
+                >
+                  <Input
+                    placeholder="Enter CNIC"
+                    className="ant-input-affix-wrapper"
+                  />
+                </Form.Item>
+              </Col>
+              <Col span={mobileResponsive ? 24 : 12}>
+                <Form.Item
+                  name="email"
+                  label="Email"
+                  rules={[
+                    { required: true, message: "please enter you valid email" },
+                  ]}
+                >
+                  <Input
+                    placeholder="Enter email"
+                    className="ant-input-affix-wrapper"
+                  />
+                </Form.Item>
+              </Col>
+              <Col span={mobileResponsive ? 24 : 12}>
+                <Form.Item
+                  name="phoneNumber"
+                  label="Phone Number"
+                  rules={[
+                    {
+                      required: true,
+                      message: "please enter you phone number",
+                    },
+                  ]}
+                >
+                  <Input
+                    type="number"
+                    placeholder="Enter phone number"
+                    className="ant-input-affix-wrapper"
+                  />
+                </Form.Item>
+              </Col>
+              <Col span={24} className="textarea">
+                <Form.Item
+                  name="description"
+                  label="Description"
+                  rules={[
+                    { required: true, message: "please enter you description" },
+                  ]}
+                >
+                  <TextArea
+                    rows={4}
+                    placeholder="Enter description"
+                    className="ant-input-affix-wrapper"
+                  />
+                </Form.Item>
+              </Col>
+            </Row>
+            <div span={24} className="form-button">
+              <button>Send message</button>
+            </div>
+          </Form>
+        </div>
+      </div>
 
-export default Contact
+      {/* Meet Our iSTA Team */}
+
+      <div className="meet">
+        <div className="meet-team">
+          <div className="meet-box">
+            <p className="meet-our">Frequently Asked Questions</p>
+            <p className="meet-explor">Explore</p>
+          </div>
+        </div>
+      </div>
+
+      <Footer />
+    </div>
+  );
+};
+
+export default Contact;
